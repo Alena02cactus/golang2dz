d631b87e5f92ff8d066826eb2c15d9f390ec4486
Alena <m1nima02@yandex.ru>
2024-05-13 19:05:03 +0300

    набросок решения

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -12,4 +13,19 @@ func main() {
 	text, _ := reader.ReadString('\n')
 
 	// Вставьте ваш код здесь
+	letterCount := make(map[rune]int)
+	totalLetters := 0
+
+	for _, char := range strings.ToLower(text) {
+		if char >= 'a' && char <= 'z' {
+			letterCount[char]++
+			totalLetters++
+		}
+	}
+
+	fmt.Println("Результат:")
+	for letter, count := range letterCount {
+		frequency := float64(count) / float64(totalLetters) * 100
+		fmt.Printf("%c - %d %.2f\n", letter, count, frequency)
+	}
 }
